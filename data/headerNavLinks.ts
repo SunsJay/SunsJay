96d04f54010007d18e139fb672d905f701e8ec83
SunsJay <SunsJay0806@gmail.com>
2025-02-24 21:01:52 +0800

    style: delete templeate file

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,11 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/blog', title: '文章' },
+  { href: '/tags', title: '标签' },
+  { href: '/projects', title: '项目' },
+  { href: '/learn', title: '学习' },
+  { href: '/sports', title: '运动' },
+  { href: '/about', title: '关于我' },
 ]
 
 export default headerNavLinks
